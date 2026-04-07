f87c44f9386c1d096b6973d2e5c5609d4b99dfc8
Avishi Sharma <robloxpusheen8@gmail.com>
2026-04-07 19:08:47 -0400

    iframe to display different hackathonsgit add .

--- app/src/lib/comps/experiences.svelte
+++ app/src/lib/comps/experiences.svelte
@@ -18,18 +18,56 @@
     import sleepover from "$lib/personalwebsite_imgs/hackathons_ysws/sleepover.webp";
     import hackforhumanity from "$lib/personalwebsite_imgs/hackathons_ysws/hackforhumanity.png";
 
-    let hackathonsList:Record<string,any>={
-        "Hack for Humanity.exe":hackforhumanity,
-        "Parthenon.exe":parthenon,
-        "Blueprint 2025.exe":bp25,
-        "Counterspell Boston.exe":counterspell,
-        "DVHacks.exe":dvhacks,
-        "saycheese.exe":saycheese,
-        "High Seas.exe":highseas,
-        "KuriusHacks.exe":kuriushacks,
-        "Blueprint 2024.exe":bp24,
-        "Wonderland.exe":wonderland,
-        "Chargerhacks.exe":charger
+    type hackathon = {
+        image:any,
+        link:string
+    }
+
+    let hackathonsList:Record<string,hackathon>={
+        "Hack for Humanity.exe":{
+            image:hackforhumanity,
+            link:"https://hack-for-humanity-26.devpost.com/"
+        },
+        "Parthenon.exe":{
+            image:parthenon,
+            link:"https://parthenon.hackclub.com/"
+        },
+        "Blueprint 2025.exe":{
+            image:bp25,
+            link:"https://bparchive.hackmit.org/2025/"
+        },
+        "Counterspell Boston.exe":{
+            image:counterspell,
+            link:"https://counterspell.hackclub.com/"
+        },
+        "DVHacks.exe":{
+            image:dvhacks,
+            link:"https://dvhacks25.devpost.com/"
+        },
+        "saycheese.exe":{
+            image:saycheese,
+            link:"https://saycheese.hackclub.com/"
+        },
+        "High Seas.exe":{
+            image:highseas,
+            link:"https://highseas.hackclub.com/"
+        },
+        "KuriusHacks.exe":{
+            image:kuriushacks,
+            link:"https://kuriushacks-march-edition.devpost.com/"
+        },
+        "Blueprint 2024.exe":{
+            image:bp24,
+            link:"https://bparchive.hackmit.org/2024/"
+        },
+        "Wonderland.exe":{
+            image:wonderland,
+            link:"https://wonderland.hackclub.com/"
+        },
+        "Chargerhacks.exe":{
+            image:charger,
+            link:"https://chargerhacks-2023.devpost.com/"
+        }
     }
 
     let visibility:string = $state("display:none");
@@ -37,8 +75,14 @@
     let hackathons:any;
     let others:any;
 
+    let iframe:any;
+
     let fileDirectoryVisibility=$state("display:flex");
     let hackathonVisibility=$state("display:none");
+
+    let iframeVisibility=$state("display:block");
+
+    let iframeSrc=$state("https://tailwindcss.com/docs/top-right-bottom-left");
 
     onMount(()=>{
         hackathons.onclick=showHackathons;
@@ -57,6 +101,17 @@
         hackathonVisibility="display:none";
         fileDirectoryVisibility="display:flex";
     }
+
+    function hideIframe(){
+        iframeVisibility="display:none";
+    }
+
+    function updateIframe(link:string){
+        iframeSrc=link;
+        iframe.onload=function(){
+            iframeVisibility="display:block";
+        }
+    }
 </script>
 
 <div class="w-[100%] h-[92vh] box-border p-[15px] bg-[#49343fff]" id="skillsFolder" style={visibility}>
@@ -71,13 +126,19 @@
         </div>
     </div>
 
-    <div style={hackathonVisibility}>
+    <div style={hackathonVisibility} class="relative">
         <button onclick={returnFiles} class="text-white courier">&lt;&lt; back</button>
         <br>
         <div class="grid grid-cols-4">
             {#each Object.keys(hackathonsList) as hackathon}
-                <HackathonIcon iconImage={hackathonsList[hackathon]} iconName={hackathon}></HackathonIcon>
+                <button onclick={function(){updateIframe(hackathonsList[hackathon].link)}}>
+                    <HackathonIcon iconImage={hackathonsList[hackathon].image} iconName={hackathon}></HackathonIcon>
+                </button>
             {/each}
+        </div>
+        <div class="fixed w-[70%] h-[90%] top-[5%] left-[50%] transform translate-x-[-50%] translate-y-[-5%] bg-[#1d2429] border-[2px] border-[#abf1f7] rounded-[20px]" style={iframeVisibility}>
+            <button class="text-[#abf1f7] ml-[95.5%] mt-[0.125%] text-[25px] mplus" onclick={hideIframe}>x</button>
+            <iframe bind:this={iframe} src={iframeSrc} class="select-none w-[100%] h-[92.5%] absolute bottom-0 left-[50%] transform translate-x-[-50%] rounded-b-[20px]" title="freaky"></iframe>
         </div>
     </div>
 </div>
