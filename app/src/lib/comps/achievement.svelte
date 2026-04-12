839cd5aa5e61affb010a38e464507115b9fcae4e
Avishi Sharma <robloxpusheen8@gmail.com>
2026-04-11 23:43:46 -0400

    little bit more responsive css +fixes

--- app/src/lib/comps/achievement.svelte
+++ app/src/lib/comps/achievement.svelte
@@ -5,9 +5,9 @@
 <div class="flex justify-around bg-[#291d23] hover:bg-[#1C1418] border-[1.5px] border-[#abf1f7] rounded-[20px] box-border p-[15px] h-[130px]">
     <img class="object-contain w-[100px]" src={img} alt={achievementName}>
     <div class="w-[70%]">
-        <h1 class="text-[#fae6f0] mplus">{achievementName}</h1>
-        <h2 class="text-[#fae6f0] mplus">{placement}</h2>
-        <h3 class="text-[#fae6f0] mplus">{year}</h3>
+        <h1 class="text-[#fae6f0] mplus md:text-[18px] text-[1.9vw]">{achievementName}</h1>
+        <h2 class="text-[#fae6f0] mplus md:text-[18px] text-[1.9vw]">{placement}</h2>
+        <h3 class="text-[#fae6f0] mplus md:text-[18px] text-[1.9vw]">{year}</h3>
     </div>
 </div>
 
--- app/src/lib/comps/langIcon.svelte
+++ app/src/lib/comps/langIcon.svelte
@@ -2,7 +2,7 @@
     let { iconName, iconImg } = $props();
 </script>
 
-<div class="text-center rounded-[25px] w-[150px] box-border p-[10px] bg-[#49343f] leading-[50px] hover:bg-[#614654]">
-    <img src={iconImg} alt="html" class="w-[100px] h-[100px] m-auto">
-    <h3 class="mplus text-white">{iconName}</h3>
+<div class="text-center rounded-[25px] md:w-[150px] w-[15vw] box-border p-[10px] bg-[#49343f] leading-[50px] hover:bg-[#614654]">
+    <img src={iconImg} alt="html" class="md:w-[100px] md:h-[100px] w-[12vw] h-[12vw] m-auto">
+    <h3 class="mplus text-white md:text-[18px] text-[1.8vw]">{iconName}</h3>
 </div>
